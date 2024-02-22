1b9b08957bbd952bb1fac366832da56a2f2d725f
paskanov <v.paskanov@gmail.com>
2024-02-22 11:49:26 +0300

    Create some new components

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {Job} from "./pages/job/job";
+import {Layout} from "./components/layout/layout";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+        <Layout/>
  <Job/>
     </>
   )
